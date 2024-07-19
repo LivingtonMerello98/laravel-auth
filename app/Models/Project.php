764ee6704c8b40de99caa8d654f26db20a2096bd
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-19 20:27:23 +0200

    [upd] slug set in table projects,Project model use Illuminate\Support\Str;

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'url', 'image', 'title', 'description', 'languages'
+        'url', 'image', 'title', 'description', 'languages', 'slug'
     ];
 
     public function getLanguagesArrayAttribute()
@@ -21,5 +22,19 @@ class Project extends Model
     public function setLanguagesArrayAttribute($value)
     {
         $this->attributes['languages'] = implode(',', $value);
+    }
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($project) {
+            $project->slug = Str::slug($project->title);
+        });
+
+        static::updating(function ($project) {
+            $project->slug = Str::slug($project->title);
+        });
     }
 }
--- database/migrations/2024_07_19_180314_add_slug_toprojects_table.php
+++ database/migrations/2024_07_19_180314_add_slug_toprojects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->string('slug')->unique()->after('title');
+            $table->string('slug', 200)->after('title');
         });
     }
 
