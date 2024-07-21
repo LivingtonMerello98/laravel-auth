08be2d0adfcdeb853cd63d3ab0032ab0695db43f
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-21 18:44:19 +0200

    [upd] dashboard fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,8 +4,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col mt-4">
-                <div class="card bg-dark">
-                    <div class="card-header text-white bg-primary bg-gradient ">{{ __('Dashboard') }}</div>
+                <div class="card bg-dark mb-5">
+                    <div class="card-header text-white bg-dark bg-gradient ">{{ __('Dashboard') }}</div>
 
                     <div class="card-body bg-secondary">
                         @if (session('status'))
@@ -15,8 +15,15 @@
                         @endif
 
                             <div class="text-warning">
-                                {{ __('You are logged in!') }}
+                                {{ __('Sei loggato!') }}
                             </div>
+                    </div>
+                </div>
+                <div class="card bg-dark">
+                    <div class="card-header text-white bg-primary bg-gradient ">Bentornato</div>
+
+                    <div class="card-body bg-secondary">
+                       <p class="text-white">Esplora i progetti, modifica e crea in tutta libertà</p>
                     </div>
                 </div>
             </div>
