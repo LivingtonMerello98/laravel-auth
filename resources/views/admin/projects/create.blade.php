693062f5c1ba3a2f464e27ee013f631d4c9eacd6
LivingtonMerello98 <livington.merello@gmail.com>
2024-07-21 18:27:51 +0200

    [upd] layout fixed in: edit, show, create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -2,38 +2,48 @@
 
 
 @section('content')
-    <div class="container">
-        <h3 class="text-white text-uppercase mb-3">crea nuovo progetto</h3>
-        <form action="{{ route('projects.store') }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <div class="form-group mb-3">
-                    <label for="url" class="text-white">URL del sito</label>
-                    <input type="text" class="form-control" id="url" name="url" placeholder="url del sito" required>
+    <div class="container py-5 mx-3">
+
+            <h3 class="text-white text-uppercase mb-3">crea nuovo progetto</h3>
+            <form action="{{ route('projects.store') }}" method="POST">
+                @csrf
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group mb-3">
+                        <label for="url" class="text-white">URL del sito</label>
+                        <input type="text" class="form-control" id="url" name="url" placeholder="url del sito" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="image" class="text-white">url dell immagine</label>
+                        <input type="text" name="image" id="thumb" class="form-control" placeholder="url dell'immagine" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="title" class="text-white">Title</label>
+                        <input type="text" name="title" id="title" class="form-control"  placeholder="nome del sito" required>
+                    </div>
+                </div>
+
+                <div class="col-md-6 ">
+                    <div class="form-group mb-3">
+                        <label for="description" class="text-white">Description</label>
+                        <textarea name="description" id="description" class="form-control" rows="4" placeholder="descrizione"></textarea>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="languages" class="text-white">Languages</label>
+                        <select  class="form-control" id="languages" name="languages[]">
+                            <option value="PHP">PHP</option>
+                            <option value="JavaScript">JavaScript</option>
+                            <option value="HTML">HTML</option>
+                            <option value="CSS">CSS</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <button type="submit" class="btn btn-primary mt-3">Create</button>
                 </div>
             </div>
-            <div class="form-group mb-3">
-                <label for="image" class="text-white">url dell immagine</label>
-                <input type="text" name="image" id="thumb" class="form-control" placeholder="url dell'immagine" required>
-            </div>
-            <div class="form-group mb-3">
-                <label for="title" class="text-white">Title</label>
-                <input type="text" name="title" id="title" class="form-control"  placeholder="nome del sito" required>
-            </div>
-            <div class="form-group mb-3">
-                <label for="description" class="text-white">Description</label>
-                <textarea name="description" id="description" class="form-control" placeholder="descrizione"></textarea>
-            </div>
-            <div class="form-group mb-3">
-                <label for="languages" class="text-white">Languages</label>
-                <select  class="form-control" id="languages" name="languages[]">
-                    <option value="PHP">PHP</option>
-                    <option value="JavaScript">JavaScript</option>
-                    <option value="HTML">HTML</option>
-                    <option value="CSS">CSS</option>
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary mt-3">Create</button>
-        </form>
+            </form>
+
     </div>
 @endsection
+
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.admin') 
 
 @section('content')
-    <div class="container">
+    <div class="container py-5">
         <div class="row">
-            <div class="col-md-6 d-flex justify-content-center align-items-center">
-                <div>
-                    <img src="{{ $project->image }}" alt="{{ $project->title }}" class="w-100" style="max-width: 200px;">
+            <div class="col-md-5 d-flex justify-content-center align-items-center">
+                <div style="width: 70%; height:auto;">
+                    <img src="{{ $project->image }}" alt="{{ $project->title }}" style="width: 100%; min-width: 50px;border-radius:0.5rem">
                 </div>
             </div>
             <div class="col-md-6">
-                <h3 class="text-white text-uppercase">{{ $project->title }}</h3>
+                <h3 class="text-white text-uppercase mb-3">{{ $project->title }}</h3>
                 <p class="text-white">{{ $project->description }}</p>
                 <p class="text-white"><strong>Languages:</strong> {{ $project->languages }}</p>
                 <a href="{{ route('projects.edit', $project->id) }}" class="btn btn-warning rounded-1">
